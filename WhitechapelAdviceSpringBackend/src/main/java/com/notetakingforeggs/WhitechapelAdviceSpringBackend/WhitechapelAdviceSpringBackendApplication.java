8c8a0325b9f3535de504af528df499d53ee9839b
ludic <notetakingforeggs@gmail.com>
2025-03-24 10:32:23 +0000

    initial commit from correct directory

---
+++ WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/WhitechapelAdviceSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notetakingforeggs.WhitechapelAdviceSpringBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitechapelAdviceSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitechapelAdviceSpringBackendApplication.class, args);
+	}
+
+}
---
+++ WhitechapelAdviceSpringBackend/src/test/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/WhitechapelAdviceSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notetakingforeggs.WhitechapelAdviceSpringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitechapelAdviceSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
