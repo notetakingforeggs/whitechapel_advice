8e9e94e63accbdd448017f101930d31b2aee5152
ludic <notetakingforeggs@gmail.com>
2025-05-09 10:56:49 +0100

    feat: court case service layer

---
+++ WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/service/CourtCaseService.java
@@ -0,0 +1,14 @@
+package com.notetakingforeggs.WhitechapelAdviceSpringBackend.service;
+
+import com.notetakingforeggs.WhitechapelAdviceSpringBackend.model.CourtCase;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CourtCaseService {
+    List<CourtCase> findByRegion(long regionId);
+    List<CourtCase> findByClaimantContainingIgnoreCase(String claimant);
+    List<CourtCase> findByDefendantContainingIgnoreCase(String defendant);
+    List<CourtCase> findByHearingTypeContainingIgnoreCase(String hearingType);
+}
---
+++ WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/service/CourtCaseServiceImpl.java
@@ -0,0 +1,35 @@
+package com.notetakingforeggs.WhitechapelAdviceSpringBackend.service;
+
+import com.notetakingforeggs.WhitechapelAdviceSpringBackend.model.CourtCase;
+import com.notetakingforeggs.WhitechapelAdviceSpringBackend.repository.CourtCaseRepository;
+
+import java.util.List;
+
+public class CourtCaseServiceImpl implements  CourtCaseService{
+
+    private final CourtCaseRepository repository;
+
+    public CourtCaseServiceImpl(CourtCaseRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<CourtCase> findByRegion(long regionId) {
+        return repository.findByRegion(regionId);
+    }
+
+    @Override
+    public List<CourtCase> findByClaimantContainingIgnoreCase(String claimant) {
+        return repository.findByClaimantContainingIgnoreCase(claimant);
+    }
+
+    @Override
+    public List<CourtCase> findByDefendantContainingIgnoreCase(String defendant) {
+        return repository.findByDefendantContainingIgnoreCase(defendant);
+    }
+
+    @Override
+    public List<CourtCase> findByHearingTypeContainingIgnoreCase(String hearingType) {
+        return repository.findByHearingTypeContainingIgnoreCase(hearingType);
+    }
+}
