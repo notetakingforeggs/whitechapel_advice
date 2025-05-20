98dbb6d91ef68ab2ecbfc62858759f78267fbf50
ludic <notetakingforeggs@gmail.com>
2025-05-20 16:18:53 +0100

    feat: basic telegram bot running

--- WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/WhitechapelAdviceSpringBackendApplication.java
+++ WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/WhitechapelAdviceSpringBackendApplication.java
@@ -1,13 +1,24 @@
 package com.notetakingforeggs.WhitechapelAdviceSpringBackend;
 
+import com.notetakingforeggs.WhitechapelAdviceSpringBackend.bot.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class WhitechapelAdviceSpringBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WhitechapelAdviceSpringBackendApplication.class, args);
+		var context = SpringApplication.run(WhitechapelAdviceSpringBackendApplication.class, args);
+
+		try{
+			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(context.getBean(TelegramBot.class));
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
---
+++ WhitechapelAdviceSpringBackend/src/main/java/com/notetakingforeggs/WhitechapelAdviceSpringBackend/bot/TelegramBot.java
@@ -0,0 +1,60 @@
+package com.notetakingforeggs.WhitechapelAdviceSpringBackend.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final String username;
+
+    public TelegramBot(@Value("${bot.token}") String token, @Value("${bot.username}") String username) {
+        super(token);
+        this.username = username;
+
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String chatId = update.getMessage().getChatId().toString();
+            String messageText = update.getMessage().getText().trim().toLowerCase();
+
+            if(messageText.trim().startsWith("claimaint:")){
+
+                sendMessage(chatId, "you said claimant");
+            } else if (messageText.trim().startsWith("defendant:")) {
+                sendMessage(chatId, "you said defendant");
+            }else{
+                String helpText = """ 
+                        Welcome to the advice service Telegram Bot!
+                        to sign up for alerts based on claimaint, type claimaint: followed by the name or names of the claimant you wish to be alerted for. To sign up for alerts based on names of defendants send a message like defendant:nameofdefendant.                        """;
+                sendMessage(chatId, helpText);
+            }
+        }
+
+    }
+
+    public void sendMessage(String chatId, String text){
+        SendMessage message = new SendMessage(chatId, text);
+        try{
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+}
